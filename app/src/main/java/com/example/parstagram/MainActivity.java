4a420b6253e0a2c64d17c10c490898c87cc3cdfd
pedrojus <p.jusino@hotmail.com>
2021-10-12 22:03:03 -0500

    initial commit

---
+++ app/src/main/java/com/example/parstagram/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.parstagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
+    private EditText etDescription;
+    private Button btnCaptureImage;
+    private ImageView ivPostImage;
+    private Button btnSubmit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etDescription = findViewById(R.id.etDescription);
+        btnCaptureImage = findViewById(R.id.btnCaptureImage);
+        ivPostImage = findViewById(R.id.ivPostImage);
+        btnSubmit = findViewById(R.id.btnSubmit);
+    }
+}
---
+++ app/src/main/java/com/example/parstagram/ParseApplication.java
@@ -0,0 +1,20 @@
+package com.example.parstagram;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+public class ParseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("sTmRutKdyfXkNgvE7LothbN1VQGSFMdNq3l14i1v")
+                .clientKey("tuSiigno03qJcMfbaS0FTWYtk6OZKRwHUpv9nr6E")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
+}
